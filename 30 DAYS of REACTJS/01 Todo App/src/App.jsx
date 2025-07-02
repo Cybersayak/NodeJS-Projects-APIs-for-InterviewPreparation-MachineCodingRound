7c9606222aa3534686ce038dcee626aca8176e25
Cybersayak <sayakcse2@gmail.com>
2025-07-02 23:40:37 +0530

    Todo in React & Rest API in Node

---
+++ 30 DAYS of REACTJS/01 Todo App/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [todos, settodos] = useState(0)
+
+  return (
+    <>
+      <div className='min-h-screen flex items-center justify-center bg-gray-100'>
+        <h1
+        </div>
+    </>
+  )
+}
+
+export default App
